5423f24694016007d2768c0beb9e942bbbc4b296
HyperShark25 <mt82841@gmail.com>
2025-05-13 18:55:12 +0300

    Deleted the dashboard folder from src folder and instead relied on the dashboard folder at the common folder

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
     <div>
       <Heading title="Dashboard" description="view Dashbord">
       </Heading>
-      <StudentDashboard />
+      <AdminDashboard />
     </div>
   );
 };
